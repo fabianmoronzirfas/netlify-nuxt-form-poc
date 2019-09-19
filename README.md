d71fe50ffc4a36ab6c2f12c7c441bb643de2f709
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-09-19 09:33:26 +0200

    I N I T !

---
+++ README.md
@@ -0,0 +1,24 @@
+# @tsb/netlify-nuxt-form-poc
+
+Testing form submission with Netlify. See https://medium.com/@kimbjrkman/adding-netlify-forms-on-your-nuxt-website-20ffba3e5ba8
+
+> My pioneering Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div>
+      <form
+        name="contactus"
+        action="/"
+        method="post"
+        netlify
+        netlify-honeypot="bot-field"
+      >
+        <!-- Hidden input to check for bots -->
+        <input type="hidden" name="form-name" value="contactus" />
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" name="name" required />
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" name="email" required />
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <textarea name="message" required></textarea>
+        </div>
+        <button type="submit" value="Send message">Send</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
